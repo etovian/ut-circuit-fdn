b7ad7ea44af1d876ad777f6f6f3cef2282005815
Michael Green <etovian@gmail.com>
2026-03-08 16:51:08 -0600

    Builders and @RequireArgsConstructor implementation.  Removed liquibase migration to seed congregation table and added it to a dedicated script.

---
+++ src/main/resources/sql/seed-congregations.sql
@@ -0,0 +1,15 @@
+-- Seed Participating Congregations
+INSERT INTO congregation (name, description, mission) VALUES
+('Cross of Christ', 'Located in Bountiful, Utah.', 'Cross of Christ is men, women and children who know Christ and desire to make Christ known.'),
+('Redeemer', 'Located in Salt Lake City, Utah.', 'Reaching people for Jesus Christ, embracing them in a Christ-centered community, and nurturing them in God''s Word to be faithful disciples.'),
+('St. John', 'Located in Salt Lake City, Utah. An eighty-plus-year-old Lutheran Church.', 'To share the Good News of the gift of complete forgiveness of all sins and guilt through Jesus in the Word and Sacrament ministry.'),
+('Grace (Sandy)', 'Located in Sandy, Utah.', 'Proclaiming the Gospel of Christ, Nurturing Faith, Reaching out with the love of Christ, and Providing Christian Fellowship.'),
+('Grace (Spanish Fork)', 'A satellite congregation of Grace in Sandy, located in Spanish Fork, Utah.', 'Proclaiming the Gospel of Christ, Nurturing Faith, and Reaching out with the love of Christ.'),
+('Grace (Moab)', 'Located in Moab, Utah.', 'To exist as a community of faith and confession in which we are personally and corporately embraced by the love of Jesus Christ, who empowers us through His Word and Sacraments to embrace one another in a fellowship of worship, caring, learning, serving, and reaching out.'),
+('Holy Trinity (Logan)', 'Located in Logan, Utah.', 'To equip followers of Jesus to be powerful witnesses of the Gospel through their everyday lives.'),
+('St. Paul', 'Located in Ogden, Utah.', 'To respond to the Great Commission by Proclaiming the Gospel of Jesus Christ; Bringing children and families into the Christian faith and fellowship of the Church; Nurturing the Christian faith in the families of the Church; and Providing a quality of Christian education.'),
+('Christ', 'Located in Murray, Utah.', 'God''s love in Jesus Christ empowers us to grow spiritually as we worship, serve, nurture & celebrate the fellowship within our church family, while proclaiming God''s unconditional love to our community.'),
+('Holy Trinity (Riverton)', 'A mission plant of Grace Sandy, located in Riverton, Utah.', 'Committed to the spreading of the Gospel, good news, of our Savior Jesus Christ who was crucified and rose again for our salvation.'),
+('Trinity', 'Located in St. George, Utah.', 'Seeks ways to share the Gospel of Jesus Christ, His death and resurrection, with the people in Washington County, throughout the state of Utah, and around the world.'),
+('Good Shepherd', 'Located in Richfield, Utah.', 'Experiencing the Love of God through Christ by: Seeking God''s will through His Word; Sharing our Lord''s love and forgiveness; and Serving and praising our Lord in prayer.'),
+('First', 'Located in Tooele, Utah. Focuses on historic Christian orthodoxy and the historical liturgy.', 'Sharing the Gospel.');
