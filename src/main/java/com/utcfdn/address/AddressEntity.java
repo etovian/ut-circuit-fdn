5245911b3b406976905ee59a3012ddfe213981cf
Michael Green <etovian@gmail.com>
2026-03-08 16:59:35 -0600

    Addresses

---
+++ src/main/java/com/utcfdn/address/AddressEntity.java
@@ -0,0 +1,41 @@
+package com.utcfdn.address;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AddressEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "street_address", nullable = false)
+    private String streetAddress;
+
+    @Column(name = "address_line_2")
+    private String addressLine2;
+
+    @Column(name = "city", nullable = false, length = 100)
+    private String city;
+
+    @Column(name = "state", nullable = false, length = 2)
+    private String state;
+
+    @Column(name = "zip_code", nullable = false, length = 10)
+    private String zipCode;
+
+    public AddressEntity(String streetAddress, String city, String state, String zipCode) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+}
